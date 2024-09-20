3e094f67bc09e12630bcb697815eac219fd63e77
kurt wokoek <kur_wokoek1@baylor.edu>
2024-09-20 15:04:42 -0500

    updated styling

--- app/(pages)/NewImage.jsx
+++ app/(pages)/NewImage.jsx
@@ -5,13 +5,19 @@ import LeftArrowButton from '../../components/buttons/LeftArrowButton'
 import RightArrowButton from '../../components/buttons/RightArrowButton'
 
 const NewImage = () => {
-    const [colorIndex, setColorIndex] = useState(0);
+    const [circleColorIndex, setCircleColorIndex] = useState(0);
+    const [squareColorIndex, setSquareColorIndex] = useState(0);
     return (
         <SafeAreaView style={styles.container}>
             <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                <LeftArrowButton numItems={colors.length} state={colorIndex} setState={setColorIndex} width={150} height={150}/>
-                <Image source={require('../../assets/shapes/circle.png')} style={{ width: 150, height: 150, resizeMode: 'contain' }} tintColor={colors[colorIndex].hex}/>
-                <RightArrowButton numItems={colors.length} state={colorIndex} setState={setColorIndex} width={150} height={150}/>
+                <LeftArrowButton numItems={colors.length} state={circleColorIndex} setState={setCircleColorIndex} width={150} height={150}/>
+                <Image source={require('../../assets/shapes/circle.png')} style={{ width: 150, height: 150, resizeMode: 'contain' }} tintColor={colors[circleColorIndex].hex}/>
+                <RightArrowButton numItems={colors.length} state={circleColorIndex} setState={setCircleColorIndex} width={150} height={150}/>
+            </View>
+            <View style={{flexDirection: 'row', alignItems: 'center', marginTop: 50}}>
+                <LeftArrowButton numItems={colors.length} state={squareColorIndex} setState={setSquareColorIndex} width={150} height={150}/>
+                <Image source={require('../../assets/shapes/square.png')} style={{ width: 150, height: 150, resizeMode: 'contain' }} tintColor={colors[squareColorIndex].hex}/>
+                <RightArrowButton numItems={colors.length} state={squareColorIndex} setState={setSquareColorIndex} width={150} height={150}/>
             </View>
         </SafeAreaView>
     )
